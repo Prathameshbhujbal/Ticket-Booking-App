3ec0322dac4774cdb370b50ba7b0b8795a693ae4
Prathameshbhujbal <bhujbal.prad@gmail.com>
2023-04-02 20:09:01 +0530

    added admin app routes

--- application/controllers.py
+++ application/controllers.py
@@ -13,6 +13,30 @@ def user_login():
 def user_dashboard():
     return render_template("user_dashboard.html")
 
+@app.route("/user_bookings")
+def user_bookings():
+    return render_template("user_bookings.html")
+
+@app.route("/user_seat_selection")
+def user_seat_selection():
+    return render_template("user_seat_selection.html")
+
+@app.route("/admin_login")
+def admin_login():
+    return render_template("admin_login.html")
+
 @app.route("/admin_dashboard")
 def admin_dashboard():
     return render_template("admin_dashboard.html")
+
+@app.route("/admin_show_summary")
+def admin_show_summary():
+    return render_template("admin_show_summary.html")
+
+@app.route("/admin_shows")
+def admin_shows():
+    return render_template("admin_shows.html")
+
+@app.route("/admin_venues")
+def admin_venues():
+    return render_template("admin_venues.html")
